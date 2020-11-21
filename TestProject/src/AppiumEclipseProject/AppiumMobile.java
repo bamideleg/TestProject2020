11a53615bc2383b65652e44ecbc0dd480bd4e2fb
deledpe <deledpe@gmail.com>
2020-11-21 21:47:13 +0000

    Mobile Testing code cmmit

---
+++ TestProject/src/AppiumEclipseProject/AppiumMobile.java
@@ -0,0 +1,16 @@
+package AppiumEclipseProject;
+
+
+import org.testng.annotations.Test;
+
+public class AppiumMobile {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test 1");
+
+	}
+
+}
---
+++ TestProject/src/GitLoad/Google_login.java
@@ -0,0 +1,13 @@
+
+// Account: bamideleg@live.com; Musediku1
+
+
+package GitLoad;
+
+public class Google_login {
+
+	public static void main(String[] args) {
+		System.out.println("Hiiiiiiiii world 123 ");
+	}
+
+}
